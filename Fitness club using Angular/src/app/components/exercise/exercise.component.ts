c9dbc1078afe25146fe5eae5f20537782eb2aab1
Pallela_Kalyan <100932107+PallelaKalyan@users.noreply.github.com>
2024-03-06 14:36:56 +0530

    Add files via upload

---
+++ Fitness club using Angular/src/app/components/exercise/exercise.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+/* @figmaId 929:192 */
+@Component({
+  selector: 'cl-exercise',
+  templateUrl: './exercise.component.html',
+  styleUrls: ['./exercise.component.css'],
+})
+export class ExerciseComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
